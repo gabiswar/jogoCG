399c979b3a79b087d635686edaf8467711d4f3b1
gabiswar <108243030+gabiswar@users.noreply.github.com>
2023-06-11 12:58:45 -0300

    Add files via upload

---
+++ jogoDeRocha/jogo.cpp
@@ -0,0 +1,367 @@
+#include "jogo.h"
+#include <iostream>
+#include <math.h>
+#include <bits/stdc++.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+int windowW = 800;
+int windowH = 600;
+
+//--------------Inimigo Cena 1
+float squareSize = 50.0f;
+float squarePosX = (windowW - squareSize) / 2;
+float squarePosY = (windowH - squareSize) / 2;
+
+//--------------Tiros
+float bulletRadius = 10.0f;
+float bulletPosX[4]; // Array para armazenar as posi��es x dos tiros
+float bulletPosY[4]; // Array para armazenar as posi��es y dos tiros
+float bulletSpeed = 2.0f;
+float bulletAngle[4]; // Array para armazenar os �ngulos dos tiros
+
+//----------Personagem
+float X = 450;
+float Y = 20;
+float comp = 103.2;
+float alt = 177.9;
+
+//--------------Pontos
+float pointSize = 30.0f;
+vector<float> pointPosX;
+vector<float> pointPosY;
+int pontos = 0;
+
+//--------------Variaveis de controle
+bool pause = false;
+bool esq = false;
+bool dir = false;
+bool cima = false;
+bool baixo = false;
+
+void drawPoints() {
+    // Confi do texto
+    glColor3f(1.0f, 1.0f, 1.0f);
+    float altura = (windowH + 200) /2;
+    glRasterPos2f((windowW - 420)/2 , altura);
+
+    // Exibe o passo atual na tela
+    std::string text;
+    text = "0 / 100\n";
+
+    float textPosition = altura;
+    // Exibe o texto na tela
+    for (const char c : text) {
+        if (c == '\n') {
+            textPosition = textPosition - 30;
+            glRasterPos2f((windowW - 420)/2, textPosition);
+        } else {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+        }
+    }
+
+    for (size_t i = 0; i < pointPosX.size(); i++) {
+        glBegin(GL_QUADS);
+        glVertex2f(pointPosX[i], pointPosY[i]);
+        glVertex2f(pointPosX[i] + pointSize, pointPosY[i]);
+        glVertex2f(pointPosX[i] + pointSize, pointPosY[i] + pointSize);
+        glVertex2f(pointPosX[i], pointPosY[i] + pointSize);
+        glEnd();
+    }
+}
+
+void initPontos() {
+    // Inicializa os pontos
+    pointPosX.push_back(100.0f);
+    pointPosY.push_back(200.0f);
+    pointPosX.push_back(300.0f);
+    pointPosY.push_back(400.0f);
+}
+
+void contaPonto() {
+    // Confi do texto
+    glColor3f(1.0f, 1.0f, 1.0f);
+    float altura = (windowH + 200) /2;
+    glRasterPos2f((windowW - 420)/2 , altura);
+
+    // Exibe o passo atual na tela
+    std::string text;
+    text += pontos + " / 100\n";
+
+    float textPosition = altura;
+    // Exibe o texto na tela
+    for (const char c : text) {
+        if (c == '\n') {
+            textPosition = textPosition - 30;
+            glRasterPos2f((windowW - 420)/2, textPosition);
+        } else {
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c);
+        }
+    }
+}
+
+void drawInimigoLevel1() {
+    glBegin(GL_QUADS);
+    glVertex2f(squarePosX, squarePosY);
+    glVertex2f(squarePosX + squareSize, squarePosY);
+    glVertex2f(squarePosX + squareSize, squarePosY + squareSize);
+    glVertex2f(squarePosX, squarePosY + squareSize);
+    glEnd();
+}
+
+void drawTiro(int index) {
+    glPushMatrix();
+    glTranslatef(bulletPosX[index], bulletPosY[index], 0.0f);
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(0.0f, 0.0f);
+    for (int i = 0; i <= 360; i += 10) {
+        float angle = i * 3.14159f / 180.0f;
+        float x = bulletRadius * cos(angle);
+        float y = bulletRadius * sin(angle);
+        glVertex2f(x, y);
+    }
+    glEnd();
+    glPopMatrix();
+}
+
+void initTiros() {
+    // Inicializa as posi��es dos tiros
+    float diagonalOffset = squareSize * sqrt(2) / 2; // Dist�ncia entre o centro do quadrado e as diagonais
+    bulletPosX[0] = squarePosX + squareSize / 2 + diagonalOffset;
+    bulletPosY[0] = squarePosY + squareSize / 2 + diagonalOffset;
+    bulletPosX[1] = squarePosX + squareSize / 2 - diagonalOffset;
+    bulletPosY[1] = squarePosY + squareSize / 2 + diagonalOffset;
+    bulletPosX[2] = squarePosX + squareSize / 2 + diagonalOffset;
+    bulletPosY[2] = squarePosY + squareSize / 2 - diagonalOffset;
+    bulletPosX[3] = squarePosX + squareSize / 2 - diagonalOffset;
+    bulletPosY[3] = squarePosY + squareSize / 2 - diagonalOffset;
+
+    // Inicializa os �ngulos dos tiros
+    bulletAngle[0] = 0.0f;
+    bulletAngle[1] = 0.5f * M_PI;
+    bulletAngle[2] = M_PI;
+    bulletAngle[3] = 1.5f * M_PI;
+}
+
+void updateTiro(int index) {
+    bulletPosX[index] += bulletSpeed * cos(bulletAngle[index]);
+    bulletPosY[index] += bulletSpeed * sin(bulletAngle[index]);
+
+    // Verifica se o tiro chegou ao final da tela
+    if (bulletPosX[index] < 0 || bulletPosX[index] > windowW || bulletPosY[index] < 0 || bulletPosY[index] > windowH) {
+        // Reposiciona o tiro no centro da tela
+        bulletPosX[index] = windowW / 2;
+        bulletPosY[index] = windowH / 2;
+    }
+
+    // Verifica a colisão entre o tiro e o personagem
+    if (bulletPosX[index] >= X && bulletPosX[index] <= X + comp && bulletPosY[index] >= Y && bulletPosY[index] <= Y + alt) {
+        // O tiro colidiu com o personagem
+        printf("COLIDIU");
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // Muda a cor do fundo para verde
+    }
+}
+
+void teclaPressionada(unsigned char key, int x, int y) {
+    switch (key) {
+    case 80: //P
+    case 112: //p
+        printf("P\n");
+        break;
+
+    case 87: //w
+    case 119: //W
+    	if (cima == false) {
+		cima = true;
+		baixo = false;
+    	}
+        break;
+
+    case 83: //s
+    case 115: //S
+    	if (baixo == false) {
+		cima = false;
+		baixo = true;
+    	}
+        break;
+
+    case 65: //a
+    case 97: //A
+    	if (esq == false) {
+		esq = true;
+		dir = false;
+    	}
+        break;
+    case 68: //d
+    case 100: //D
+    	if(dir == false) {
+		esq = false;
+		dir = true;;
+    	}
+        break;
+
+    case 27:      // Tecla "ESC"
+        exit(0);  // Sai da aplicação
+        break;
+
+    default:
+        break;
+    }
+
+    glutPostRedisplay();
+}
+
+void teclaSolta(unsigned char key, int x, int y) {
+    switch (key) {
+        case 87: //w
+        case 119: //W
+            cima = false;
+            break;
+
+        case 83: //s
+        case 115: //S
+            baixo = false;
+            break;
+
+        case 65: //a
+        case 97: //A
+            esq = false;
+            break;
+
+        case 68: //d
+        case 100: //D
+            dir = false;
+            break;;
+
+        /*case 80: //P
+            printf("SAI no P");
+            break;
+        case 68: //p
+            printf("SAI no d");
+            break;
+        default:
+            break;*/
+    }
+    glutPostRedisplay();
+}
+
+void renderCenaFase1(GLuint background, GLuint personagem) {
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // Define a cor de fundo como branco
+    glClear(GL_COLOR_BUFFER_BIT);
+     // -------------------------- background
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, background);
+	glBegin(GL_TRIANGLE_FAN);// quadrado do tamanho da tela
+	glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
+	glTexCoord2f(1, 0); glVertex3f(windowW, 0, 0);
+	glTexCoord2f(1, 1); glVertex3f(windowW, windowH, 0);
+	glTexCoord2f(0, 1); glVertex3f(0, windowH, 0);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+
+    // -------------------- Personagem
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, personagem);
+	glBegin(GL_TRIANGLE_FAN); // PERSONAGEM
+	glTexCoord2f(0, 0); glVertex3f(X, Y, 0);
+	glTexCoord2f(1, 0); glVertex3f(X + comp, Y, 0);
+	glTexCoord2f(1, 1); glVertex3f(X + comp, Y + alt, 0);
+	glTexCoord2f(0, 1); glVertex3f(X, Y + alt, 0);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+
+    //desenha os pontos.
+	drawPoints();
+
+    // Verifica colisao entre o personagem e os pontos
+    for (size_t i = 0; i < pointPosX.size(); i++) {
+        if (X < pointPosX[i] + pointSize && X + comp > pointPosX[i] && Y < pointPosY[i] + pointSize && Y + alt > pointPosY[i]) {
+            printf("Colidiu com o ponto");
+            pointPosX.clear();
+            pointPosY.clear();
+            glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // Muda a cor do fundo para azul
+            contaPonto();
+            break; // Interrompe o loop, pois já encontrou uma colisão
+        }
+    }
+
+     // Desenha a fase
+    glColor3f(0.5f, 0.5f, 0.5f);
+    drawInimigoLevel1();
+
+    // Desenha os tiros
+    for (int i = 0; i < 4; i++) {
+        drawTiro(i);
+    }
+
+    glutSwapBuffers();
+}
+
+void adicionaPonto() {
+    // Limpa os pontos existentes
+    pointPosX.clear();
+    pointPosY.clear();
+
+    float posX = fmod(rand(), (windowW - pointSize));
+    float posY = fmod(rand(), (windowH - pointSize));
+
+    pointPosX.push_back(posX);
+    pointPosY.push_back(posY);
+}
+
+void atualizaPontos(int value) {
+    adicionaPonto();
+
+    glutPostRedisplay();
+    glutTimerFunc(4000, atualizaPontos, 4000);
+}
+
+void updateScene(int value) {
+    for (int i = 0; i < 4; i++) {
+        updateTiro(i);
+    }
+
+    if (pause == false) {
+        if (esq == true) {
+            X = X - 5;
+        }
+        else if (dir == true) {
+            X = X + 5;
+        }
+        if (cima == true) {
+            Y = Y + 5;
+        }
+        else if (baixo == true) {
+            Y = Y - 5;
+        }
+        if (X < 0) {
+            esq = false;
+            X = 0;
+        }
+        else if (X > windowW - comp) {
+            dir = false;
+            X = windowW - comp;
+
+        }
+        if (Y < 0) {
+            baixo = false;
+            Y = 0;
+        }
+        else if (Y > windowH - alt) {
+            cima = false;;
+            Y = windowH - alt;
+
+        }
+    }
+
+    glutPostRedisplay();
+    glutTimerFunc(16, updateScene, 0);
+}
+
+void renderSceneJogando(GLuint fundo, GLuint personagem) {
+    renderCenaFase1(fundo, personagem);
+}
---
+++ jogoDeRocha/telaMenu.h
@@ -0,0 +1,14 @@
+#ifndef TELAMENU_H
+#define TELAMENU_H
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <SOIL/SOIL.h>
+#include <stdio.h>
+#include <iostream>
+
+void renderSceneMenu(GLuint iniciarTexture, GLuint comoJogarTexture, bool mouseCimaJ, bool mouseCimaCJ, int windowWidth, int windowHeight);
+
+void renderSceneComoJogar(GLuint voltarMenuTexture, bool mouseCimaVoltMenu, int windowWidth, int windowHeight);
+
+#endif
